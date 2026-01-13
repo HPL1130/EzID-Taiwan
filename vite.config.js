603fc60f7225b52d469665fe68d859fe71c6bb9b
HPL1130 <aircon.tw@gmail.com>
2026-01-13 17:15:52 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // 確保路徑正確
-  optimizeDeps: {
-    exclude: ['@imgly/background-removal']
-  }
+  base: '/EzID-Taiwan/', // 加上這行，對應你的 GitHub 倉庫名稱
 })
