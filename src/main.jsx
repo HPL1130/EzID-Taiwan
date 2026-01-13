4749e00c2b6560fda335cdbacbe2a985cc70c9af
HPL1130 <aircon.tw@gmail.com>
2026-01-13 17:17:40 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx' // 檢查檔名大小寫是否一致
+import App from './App.jsx' // 確保這裡的大寫 App 與檔案名稱一致
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
