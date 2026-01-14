adf8b47914443f9d6ff2659dea35aa4a46d74cff
HPL1130 <aircon.tw@gmail.com>
2026-01-14 08:07:29 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx' // 確保這裡的大寫 App 與檔案名稱一致
-import './index.css'
+import App from './App.jsx'
 
+// 刪除了 import './index.css' 這一行，因為檔案不存在且我們使用 CDN
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
