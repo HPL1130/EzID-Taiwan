ca7e5358a090ba309679a129a9bb7be323dac4d7
HPL1130 <aircon.tw@gmail.com>
2026-01-15 11:16:33 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/EzID-Taiwan/', // 加上這行，對應你的 GitHub 倉庫名稱
+  base: '/EzID-Taiwan/', // 確保部署路徑正確
+  build: {
+    outDir: 'dist',
+  }
 })
